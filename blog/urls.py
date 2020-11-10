1df2e164b05c83d6e791c512208c43233a8f2789
ExpressHermes <9876shivamyadav@gmail.com>
2020-11-10 13:07:12 +0530

    Added requirements.txt

--- blog/urls.py
+++ blog/urls.py
@@ -9,9 +9,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Blog API",
       default_version='v1',
-      description="Test description",
+      description="Blog API using Django rest framework",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
