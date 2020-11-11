55596b2bca196d06b90eff17c58105427cfb6010
ExpressHermes <9876shivamyadav@gmail.com>
2020-11-11 13:01:32 +0530

    Bug fixes

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,6 +7,8 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(read_only=True)
     password = serializers.CharField(min_length=8, max_length=32, write_only=True)
+    email = serializers.EmailField(max_length=50, allow_blank=False)
+    
     class Meta:
         model = User
         fields = ["id", "username", "email", "password"]
